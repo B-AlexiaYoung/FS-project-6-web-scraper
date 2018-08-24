222bf36531d8d0cc557a87368fabf140addc985b
B-AlexiaYoung <brlxyng@gmail.com>
2018-08-24 11:40:19 -0600

    errors

--- scraper.js
+++ scraper.js
@@ -4,7 +4,9 @@ const fs = require("fs");
 const Json2csvParser = require('json2csv').Parser;
 const fields = ["title", "price", "imageurl", "url", "time"];
 const mainurl = "http://www.shirts4mike.com/shirts.php";
-const opts = {fields};
+const opts = {
+    fields
+};
 let innerItems = [];
 let titleholder, priceholder, imageurlholder, urlholder, time;
 let shirts = [];
@@ -32,26 +34,29 @@ request(mainurl, function (err, response, html) {
     if (!err) {
         let $ = cheerio.load(html);
         let listItems = $(".products").children();
-
         listItems.each(function (index) {
             innerItems.push($(".products").children().eq(index).find("a").attr("href"));
         })
         shirtDetails();
     }
-        
+    if (err) {
+        console.log(err.message);
+        console.log(`Sorry, there seems to be a connection error:  ${err.message}`);
+        errorlog();
+    }
 
 });
 
 function shirtDetails() {
     innerItems.forEach(function (index) {
         let shirturl = "http://www.shirts4mike.com/" + (index);
-     let   shirty = shirturl;
+        let shirty = shirturl;
 
         //request("http://www.shirts4mike.com/" + (index), function (err, response, html) {
         request({
-                method : "GET",
-                uri : "http://www.shirts4mike.com/" + (index),
-                gzip : true
+                method: "GET",
+                uri: "http://www.shirts4mike.com/" + (index),
+                gzip: true
             },
             function (err, response, html) {
                 let $ = cheerio.load(html);
@@ -69,13 +74,13 @@ function shirtDetails() {
                     let timenow = time.getTime();
                     console.log(timenow);
                     shirts.push({
-                            title: titleholder,
-                            price: priceholder,
-                            imageurl: imageurlholder,
-                            url: shirturl,
-                            time: timenow
-                        })
-                    
+                        title: titleholder,
+                        price: priceholder,
+                        imageurl: imageurlholder,
+                        url: shirturl,
+                        time: timenow
+                    })
+
                     //
                     console.log(shirts);
 
@@ -84,24 +89,33 @@ function shirtDetails() {
     })
     //console.log(shirts);
 }
-function makefile () {
-    if(shirts.length>0 ){
-    console.log("write file");
-    try {
-        const parser = new Json2csvParser(opts);
-        const csv = parser.parse(shirts);
-        fs.writeFile('./data/'+ datetime +".csv", csv, (err) => {  
-            // throws an error, you could also catch it here
-            if (err) throw err;
-        
-            // success case, the file was saved
-            console.log('Lyric saved!');
-        });
-        console.log(csv);
-      } catch (err) {
-        console.error(err);
-      }
+// write to data file
+function makefile() {
+    if (shirts.length > 0) {
+        console.log("write file");
+        try {
+            const parser = new Json2csvParser(opts);
+            const csv = parser.parse(shirts);
+            fs.writeFile('./data/' + datetime + ".csv", csv, (err) => {
+                if (err) {
+                    console.log("error");
+                }
+                console.log('shirts saved');
+            });
+            console.log(csv);
+        } catch (err) {
+            console.error(err);
+        }
     }
 }
 setTimeout(makefile, 2500);
 
+function errorlog() {
+    let timestamp = Date() + "\n";
+    fs.appendFile('scraper-error.log', timestamp, function (err) {
+        console.log(timestamp)
+        if (err) {;
+            console.log("scraper error logged");
+        }
+    });
+}
